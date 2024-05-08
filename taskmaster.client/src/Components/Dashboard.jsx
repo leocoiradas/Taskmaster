7161db27d165d39ecf31fbcc15006a6effbf5af2
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:16:14 -0300

    Creado componente Dashboard

---
+++ taskmaster.client/src/Components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Children } from "react";
+
+function Dashboard() {
+    return (
+      <section className="w-full h-full flex justify-center items-center p-3 gap-5">
+            {Children }
+      </section>
+  );
+}
+
+export default Dashboard;
