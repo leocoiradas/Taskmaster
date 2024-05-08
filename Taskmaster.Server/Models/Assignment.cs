720c9c7215810e5f8db976da4ebaff1147b13071
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:46:55 -0300

    Creación de modelo de tareas

---
+++ Taskmaster.Server/Models/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taskmaster.Server.Models;
+
+public partial class Assignment
+{
+    public Guid? AssignmentID { get; set; }
+
+    public int? EmployeeAssigned { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public DateOnly CreatedAt { get; set; }
+
+    public DateOnly DueAt { get; set; }
+
+    public virtual Employee? EmployeeAssignedNavigation { get; set; }
+}
