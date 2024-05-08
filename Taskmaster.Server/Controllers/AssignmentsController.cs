6c62670b04d9680d863397ed584847700cc99146
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:47:29 -0300

    Creación de controlador de tareas

---
+++ Taskmaster.Server/Controllers/AssignmentsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Taskmaster.Server.Models;
+
+namespace Taskmaster.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentsController : ControllerBase
+    {
+        private readonly TaskmasterContext _dbcontext;
+
+        public AssignmentsController(TaskmasterContext context)
+        {
+            _dbcontext = context;
+        }
+
+        [HttpGet]
+        [Route("assignments")]
+        public async Task <IActionResult> GetAssignments()
+        {
+           List <Assignment> TasksCollections = await _dbcontext.Assignments.OrderByDescending(e => e.AssignmentID).ToListAsync();
+           return StatusCode(StatusCodes.Status100Continue);
+        }
+
+        [HttpGet]
+        [Route("assignments/{string: id}")]
+        public async Task<IActionResult> GetAssignmentById(string id)
+        {
+            Assignment TaskById = _dbcontext.Assignments.Find(id);
+            if (TaskById != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, TaskById);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            //List<Assignment> TasksCollections = await _dbcontext.Assignments.OrderByDescending(e => e.AssignmentID).ToListAsync();
+            
+        }
+        [HttpPut]
+        [Route("update")]
+        public async Task <IActionResult> UpdateAssignment([FromBody] Assignment assignmentData)
+        {
+            Assignment searchAssignment = await _dbcontext.Assignments.FindAsync(assignmentData.AssignmentID);
+            if (searchAssignment != null)
+            {
+                _dbcontext.Assignments.Update(searchAssignment);
+                await _dbcontext.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{string: id}")]
+        public async Task<IActionResult> DeleteAssignment(string id)
+        {
+            Assignment searchAssignment = await _dbcontext.Assignments.FindAsync(id);
+            if (searchAssignment != null)
+            {
+                _dbcontext.Assignments.Remove(searchAssignment);
+                await _dbcontext.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+        }
+    }
+}
