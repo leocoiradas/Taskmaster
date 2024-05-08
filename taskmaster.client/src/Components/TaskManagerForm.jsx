cfb39b3c7076f26997c00d11dce3dd04dcf62740
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:16:40 -0300

    Creado formulario para administrar la creación y edición de tareas.

---
+++ taskmaster.client/src/Components/TaskManagerForm.jsx
@@ -0,0 +1,18 @@
+function TaskManagerForm() {
+  return (
+      <section className="w-full h-full flex justify-center items-center p-3">
+          <form action="" method="post">
+              <label></label>
+              <input />
+              <label></label>
+              <input />
+              <label></label>
+              <input />
+              <label></label>
+              <input />
+          </form>
+      </section>
+  );
+}
+
+export default TaskManagerForm;
