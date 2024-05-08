118ba6ec6dd9a20c5d7aa73f66d3d4a3749bf4f5
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:46:35 -0300

    Creación de modelo de empleado

---
+++ Taskmaster.Server/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taskmaster.Server.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
