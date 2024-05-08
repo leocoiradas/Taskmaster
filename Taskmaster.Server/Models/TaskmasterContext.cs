ebe333a6b0aafbc4139d479e5e2964a275fec845
Alan Coiradas <coiradasalan@gmail.com>
2024-05-08 16:46:22 -0300

    Creación de contexto de la base de datos Taskmaster.Context.cs

---
+++ Taskmaster.Server/Models/TaskmasterContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taskmaster.Server.Models;
+
+public partial class TaskmasterContext : DbContext
+{
+    public TaskmasterContext()
+    {
+    }
+
+    public TaskmasterContext(DbContextOptions<TaskmasterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Assignment> Assignments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-6OGJ7LKA\\LEODEVELOP; DataBase=Taskmaster;TrustServerCertificate=true;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF170C9EEBE");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Role)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Assignment>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(CONVERT([date],getdate()))");
+            entity.Property(e => e.Description)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.AssignmentID)
+                .HasDefaultValueSql("(newid())")
+                .HasColumnName("AssignmentID");
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.EmployeeAssignedNavigation).WithMany()
+                .HasForeignKey(d => d.EmployeeAssigned)
+                .HasConstraintName("FK__Tasks__EmployeeA__4BAC3F29");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
