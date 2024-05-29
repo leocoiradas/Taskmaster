218ed69a0cb334ca6fbc450a889f541d8de64b48
Alan Coiradas <coiradasalan@gmail.com>
2024-05-29 13:04:57 -0300

    Program.cs: Configuración de Cors y JWT

--- Taskmaster.Server/Program.cs
+++ Taskmaster.Server/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
+using System.Text;
 using Taskmaster.Server.Models;
+using Taskmaster.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +22,43 @@ builder.Services.AddDbContext<TaskmasterContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("TaskmasterDatabase"));
 });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolitics", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+//Configuración necesaria para la autenticación con JWT
+
+builder.Services.AddScoped<IAuthorizeService, AuthorizeService>();
+
+var tokenKey = builder.Configuration.GetValue<string>("JwtSettings:key");
+var keyBytes = Encoding.ASCII.GetBytes(tokenKey);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+
+//Fin de la configuración de JWT
+
 
 /*public void ConfigureServices(IServiceCollection services)
 {
@@ -38,6 +79,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolitics");
+
+//Se hace uso de autenticación en la app
+app.UseAuthentication();
 
 app.UseAuthorization();
 
