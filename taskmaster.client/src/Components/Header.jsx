9536043b387c0f82c2f916067ca9a72807b47855
Alan Coiradas <coiradasalan@gmail.com>
2024-05-30 17:51:20 -0300

    Header: Creado Header de la aplicación

---
+++ taskmaster.client/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import HomepageTitle from "./HomepageComponents/HomepageTitle";
+
+function Header(){
+return(
+    <header className="w-full min-h-[10dvh] flex justify-start items-center p-2 bg-red-500">
+        <HomepageTitle />
+    </header>
+)}
+
+export default Header;
