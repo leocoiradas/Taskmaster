6fda05a0c0f205bdf48a11d1ce17dcebd8cfa12e
Alan Coiradas <coiradasalan@gmail.com>
2024-05-30 18:01:09 -0300

    Button: Correción de errores
    Se efectuó una desestructuración al momento de ingresar los parámetros del componente. Debido a la falta de este concepto la aplicación daba errores al momento de renderizar este componente.

--- taskmaster.client/src/Components/Button.jsx
+++ taskmaster.client/src/Components/Button.jsx
@@ -1,6 +1,6 @@
-function Button(buttonName, eventFunction, buttonColor) {
+function Button({ buttonName, eventFunction, buttonColor }) {
     return (
-        <button onClick={eventFunction} className="px-6 py-4 text-center" style={{ backgroundColor:buttonColor }}>{buttonName}</button>
+        <button onClick={eventFunction} className="px-6 py-4 text-center rounded-md text-white" style={ {backgroundColor:buttonColor} }>{buttonName}</button>
   );
 }
 
