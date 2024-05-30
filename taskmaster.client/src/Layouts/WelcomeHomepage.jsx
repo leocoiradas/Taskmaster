7e4cd0b1da426aab8035fc9048b11979b2fc9bfe
Alan Coiradas <coiradasalan@gmail.com>
2024-05-30 17:55:39 -0300

    Creación de WelcomeHomepage
    En este componente se irá desarrollando la página por defecto al abrir la aplicación por primera vez.

---
+++ taskmaster.client/src/Layouts/WelcomeHomepage.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../Components/HomepageComponents/Carousel";
+import HomepageTitle from "../Components/HomepageComponents/HomepageTitle";
+
+function WelcomeHomepage() {
+  return (
+      <section>
+        <HomepageTitle />
+      </section>
+  );
+}
+
+export default WelcomeHomepage;
