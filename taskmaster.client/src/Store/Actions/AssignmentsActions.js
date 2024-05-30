9df87af6cd7ef95628de895511614154afde3d35
Alan Coiradas <coiradasalan@gmail.com>
2024-05-30 17:49:19 -0300

    Creado AssignmentsActions
    Creado el sistema de acciones de las tareas. Haciendo uso de axios se realizan las distintas peticiones al back y se disparan con la acción correspondiente de redux.

---
+++ taskmaster.client/src/Store/Actions/AssignmentsActions.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const createAssignment = createAsyncThunk("create_assignment", async (newAssignment) => {
+    try {
+        await axios.post("http://localhost:3000", newAssignment);
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+export const getAssignments = createAsyncThunk("get_assignments", async () => {
+    try {
+        const response = await axios.get("https://localhost:5001/api/Assignments/assignments");
+        //console.log(response.data)
+        return {
+            assignmentsCollection: response.data
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+export const getAssignmentsByEmployee = createAsyncThunk("get_assignments_by_employee", async (assignmentID) => {
+    try {
+        const response = await axios.get(`http://localhost:3000/${assignmentID}`);
+        return {
+            assignmentsByEmployee: response.data
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+export const editAssignment = createAsyncThunk("edit_assignment", async (assignmentData) => {
+    try {
+        await axios.put("https://localhost:5001/api/Assignments/update", assignmentData);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export const deleteAssignment = createAsyncThunk("delete_assignment", async (assignmentID) => {
+    try {
+        await axios.post(`http://localhost:3000/${assignmentID}`);
+    } catch (error) {
+        console.log(error);
+    }
+})
