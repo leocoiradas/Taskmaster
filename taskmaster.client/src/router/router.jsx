787edd310f527b58bcd3a5709c016979029e1f96
Alan Coiradas <coiradasalan@gmail.com>
2024-05-31 22:07:24 -0300

    router: Creado sistema de rutas de la aplicación.

---
+++ taskmaster.client/src/router/router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import AssignmentCollection from "../Layouts/AssignmentCollection";
+import WelcomeHomepage from "../Layouts/WelcomeHomepage";
+import Layout from "../Layouts/Layout";
+import AssignmentsByEmployee from "../Layouts/AssignmentsByEmployee";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <WelcomeHomepage />
+    },
+    {
+        path: "/dashboard",
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <AssignmentCollection />
+
+            },
+            {
+                path: "myAssignments",
+                element: <AssignmentsByEmployee />
+            }
+        ]
+    }])
+
