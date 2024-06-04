cd3c4191317e511538f579318f79e71587e0c063
Alan Coiradas <coiradasalan@gmail.com>
2024-06-04 15:18:03 -0300

    EmployeeManagerForm: estilos en el formulario + reemplazo de select por input type radio
    - Se aplicaron estilos al formulario para hacerlo más agradable a la vista.
    -Se reemplazó el select del campo role del formulario por dos input type radio. A estos se les implementó una lógica para que al momento de querer editar los detalles de un empleado, aparezca como marcado por defecto el rol actual del empleado.
    -Por lo anterior,  al declarar el useState, se estableció el valor del campo role como "Employee" para evitar posibles errores y que aparezca por defecto si se quiere crear un nuevo empleado.

--- taskmaster.client/src/Components/EmployeesComponents/EmployeeManagerForm.jsx
+++ taskmaster.client/src/Components/EmployeesComponents/EmployeeManagerForm.jsx
@@ -11,7 +11,7 @@ function EmployeeManagerForm({ employeeDetails, closeForm }) {
         name: "",
         lastName: "",
         email: "",
-        role: ""
+        role: "Employee"
     })
 
     useEffect(() => {
@@ -58,18 +58,18 @@ function EmployeeManagerForm({ employeeDetails, closeForm }) {
         return (
             <section className="w-screen h-screen flex justify-center items-center p-3 fixed inset-0 z-50 bg-gray-500/50">
                 <form action="" method={employeeDetails ? "put" : "post"} className="w-1/2 flex flex-col justify-center items-start p-3 gap-3 text-xl bg-white rounded-md">
-                    <label htmlFor="firstName">Name</label>
-                    <input name="firstName" onChange={formData} value={employeeFormData.name} />
-                    <label htmlFor="lastName">Last Name</label>
-                    <input name="lastName" onChange={formData} value={employeeFormData.lastName} />
-                    <label htmlFor="email">Email</label>
-                    <input name="email" onChange={formData} value={employeeFormData.email} />
-                    <label htmlFor="role">Role</label>
-                    <select name="role" onChange={formData} value={employeeFormData.role}>
-                        <option value="employee" selected>Employee</option>
-                        <option value="admin">Admin</option>
-                    </select>
-                    <div>
+                    <label htmlFor="firstName" className="font-semibold">Name</label>
+                    <input name="firstName" onChange={formData} value={employeeFormData.name} className="w-full p-2 border-2 rounded-md border-black" />
+                    <label htmlFor="lastName" className="font-semibold">Last Name</label>
+                    <input name="lastName" onChange={formData} value={employeeFormData.lastName} className="w-full p-2 border-2 rounded-md border-black" />
+                    <label htmlFor="email" className="font-semibold">Email</label>
+                    <input name="email" onChange={formData} value={employeeFormData.email} className="w-full p-2 border-2 rounded-md border-black" />
+                    <label htmlFor="role" className="font-semibold">Role</label>
+                    <div className="flex gap-3">
+                        <label><input type="radio" name="role" value="Admin" checked= {employeeFormData.role === "Admin"} /> Admin</label>
+                        <label><input type="radio" name="role"  value="Employee" checked= {employeeFormData.role === "Employee"} /> Employee</label>
+                    </div>
+                    <div className="flex gap-3">
                         <Button buttonName="Send data" buttonColor="blue" eventFunction={employeeDetails ? editemployeeFormData : createemployeeFormData} />
                         <Button buttonName="Close form" buttonColor="red" eventFunction={closeForm} />
                     </div>
