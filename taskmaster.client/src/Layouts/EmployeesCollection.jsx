198e3421683c537df152542f513b73219ba026de
Alan Coiradas <coiradasalan@gmail.com>
2024-06-04 15:21:49 -0300

    Creación de EmployeesCollection
    En esta sección de la aplicación se renderizará la tabla que muestra a todos los empleados disponibles en la base de datos, con algunos detalles como rol, email, nombre, id, etc.

---
+++ taskmaster.client/src/Layouts/EmployeesCollection.jsx
@@ -0,0 +1,23 @@
+import Dashboard from "../Components/Dashboard";
+import EmployeesTable from "../Components/EmployeesComponents/EmployeesTable";
+import { Suspense, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getEmployees } from "../Store/Actions/EmployeesAction";
+
+function EmployeesCollection(){
+    const dispatch = useDispatch();
+    useEffect(()=> {
+        dispatch(getEmployees());
+    },[])
+
+    const employees = useSelector((store) => store.employeesCollection.employees)
+    console.log(employees)
+
+    return(
+        <Dashboard sectionName="Employees">
+            { employees ? <EmployeesTable employeesArr={employees} /> : null }
+        </Dashboard>
+    )
+}
+
+export default EmployeesCollection
