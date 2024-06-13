66203251ecad4480b39ae71e3bacb33f7691644d
Alan Coiradas <coiradasalan@gmail.com>
2024-06-13 11:14:44 -0300

    Botón de creación de tareas: Implementada petición para obtener empleados
    Mediante una petición get se obtiene la lista de empleados con el objetivo de tener a quien asignarle una tarea al momento de ser creada.

--- taskmaster.client/src/Components/AssignmentsComponents/CreateAssignmentButton.jsx
+++ taskmaster.client/src/Components/AssignmentsComponents/CreateAssignmentButton.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import AssignmentManagerForm from "./AssignmentManagerForm";
+import { useDispatch } from "react-redux";
+import { getEmployees } from "../../Store/Actions/EmployeesAction";
+import { useSelector } from "react-redux";
+
 
 function CreateAssignmentButton() {
     const [showForm, setShowForm] = useState(false);
+    
     const displayAssignmentForm = () => {
         setShowForm(!showForm);
     }
+
+    useEffect(() => {
+        dispatch(getEmployees())
+    }, [])
+
+    const dispatch = useDispatch()
+    const employeesCollection = useSelector((store) => store.employeesCollection.employees)
     return (
         <>
             <button className="w-[30dvw] h-[50dvh] flex flex-col justify-center items-center text-xl font-semibold border-4 border-transparent rounded-md p-2 gap-3  hover:bg-green-500 hover:border-green-500 hover:text-white"
@@ -14,7 +26,7 @@ function CreateAssignmentButton() {
                 <FaPlus className="text-3xl" />
                 <p>Create Assignment</p>
             </button>
-            {showForm ? (<AssignmentManagerForm closeForm = {displayAssignmentForm} assignmentDetails={null} />) : null}
+            {showForm ? (<AssignmentManagerForm closeForm = {displayAssignmentForm} assignmentDetails={null} employeesList={employeesCollection} />) : null}
         </>
     )
 }
