1365e439c9aab113901778bd82d789bc49f8ded3
Alan Coiradas <coiradasalan@gmail.com>
2024-06-21 18:05:44 -0300

    Formulario de tareas: Implementada validación de formularios
    Haciendo uso de React-Hook-Form y Joi para validar los campos del formulario antes de enviar la petición de editar o crear según corresponda. También se mostrarán mensajes de error dependiendo de la norma de validación que se esté incumpliendo.

--- taskmaster.client/src/Components/AssignmentsComponents/AssignmentManagerForm.jsx
+++ taskmaster.client/src/Components/AssignmentsComponents/AssignmentManagerForm.jsx
@@ -1,122 +1,122 @@
-import { useState, useEffect } from "react";
 import Button from "../Button";
 import { useDispatch } from 'react-redux';
 import { editAssignment, createAssignment } from "../../Store/Actions/AssignmentsActions";
+import Joi from 'joi';
+import { useForm } from "react-hook-form";
+import { joiResolver } from '@hookform/resolvers/joi';
 
-function AssignmentManagerForm({ closeForm, assignmentDetails, employeesList }) {
 
-  const dispatch = useDispatch();
+function AssignmentManagerForm({ closeForm, assignmentDetails, employeesList, reset }) {
 
-  const [assignmentData, setAssignmentData] = useState({
-    title: "",
-    employeeAssigned: 0,
-    description: "",
-    status: "",
-    dueAt: new Date()
-  })
 
-  useEffect(() => {
-    assignmentFormData(assignmentDetails);
-  }, [assignmentDetails])
+  const schema = Joi.object({
+    title: Joi.string().trim().required().min(4).max(200).messages({
+      "string.base": "Title should be an element of type 'text'." ,
+      "string.min": "Title length should be longer than 4 characters.",
+      "string.empty": "Please write a title for the assignment.",
+      "string.max": "Title length should be shorter than 200 characters.",
+      "any.required": "Title is required."
+    }),
+    employeeAssigned: Joi.number().required().min(1).messages({
+      "number.empty": "You must assign the task to a valid employee.",
+      "number.min": "You must select a valid employee.",
+      "any.required": "An employee must be selected."
+    }),
+    description: Joi.string().trim().required().messages({
+      "string.empty": "Describe the objectives and details of the assignment.",
+      "any.required": "Description is required."
+    }),
+    status: Joi.string().required().messages({
+      "string.empty": "Please select a valid status.",
+      "any.required": "You must select a status for the assignment."
+    }),
+    dueAt: Joi.date().required().greater("now").messages({
+      "any.required": "You must set an expiring date for the assignment.",
+      "date.greater": "The date should be greater than today.",
+      "date.base": "Please enter a valid date."
+    }),
+  });
 
-  const assignmentFormData = () => {
-    if (assignmentDetails) {
-      setAssignmentData({
+  const { register, handleSubmit, formState: { errors }, watch } = useForm({
+    resolver: joiResolver(schema),
+    defaultValues: assignmentDetails ? 
+      {
         title: assignmentDetails.title,
         employeeAssigned: assignmentDetails.employeeAssigned,
         description: assignmentDetails.description,
-        status: assignmentDetails.status,
-        dueAt: assignmentDetails.dueAt
-      });
+        status:  assignmentDetails.status,
+        dueAt: assignmentDetails.dueAt,
 
-
-    } else {
-      setAssignmentData({
+      } : 
+      {
         title: "",
         employeeAssigned: 0,
         description: "",
-        status: "",
-      })
-    }
-  }
+        status:  "",
+        dueAt: new Date().toLocaleDateString(),
 
+      }
+  })
 
+  const dispatch = useDispatch();
 
-  const formData = (event) => {
-    setAssignmentData({
-      ...assignmentData,
-      [event.target.name]: event.target.value
-      
-    });
-    console.log(assignmentData)
-  }
-
-  const editAssignmentData = async (event) => {
-    event.preventDefault();
-    try {
-      dispatch(editAssignment(assignmentData));
-      window.location.reload(false)
-    } catch (error) {
-      console.log(error)
-    }
-    
-  }
-  const newAssignmentData = async (event) => {
-    event.preventDefault();
-    try{
-      dispatch(createAssignment(assignmentData));
-      window.location.reload(false)
-    } catch (error){
-      console.log(error)
-    }
-  }
-
- 
+  const submitData = handleSubmit((data) => {
+    const formData = data;
+    formData.dueAt = new Date(formData.dueAt).toISOString().split('T')[0]
+    alert("Sending assignment data.");
+    assignmentDetails ? dispatch(editAssignment(formData)) : dispatch(createAssignment(formData));
+    console.log(formData)
+    console.log(errors);
+  })
 
   return (
     <section className="w-screen h-screen flex justify-center items-center p-3 fixed inset-0 z-50 bg-gray-500/50">
       <article className="w-full flex justify-center items-center">
-        <form action="" method={assignmentDetails ? "put" : "post"} className="w-1/2 flex flex-col justify-center items-center p-3 gap-3 text-xl bg-white rounded-md shadow-md border-2 border-slate-600">
-          <fieldset className="w-full flex flex-col gap-3">
+        <form onSubmit={submitData} className="w-1/2 max-h-screen flex flex-col justify-center items-center p-3 gap-5 text-xl bg-white rounded-md shadow-md border-2 border-slate-600">
+          <fieldset className="w-full flex flex-col gap-5">
             <legend className="text-center font-semibold">{assignmentDetails ? "Edit assignment data" : "Create assignment"}</legend>
-            <fieldset>
-              <label htmlFor="title" className="font-semibold">Title</label>
-              <input name="title" type="text" value={assignmentData.title} onChange={formData} className="w-full p-2 border-2 rounded-md border-black" required />
+            <fieldset className="flex flex-wrap">
+              <label htmlFor="title" className="flex-1 font-semibold">Title</label> {errors.title && <p className="inline text-right text-lg text-red-600">{errors.title?.message}</p>}
+              <input type="text" {...register("title")} id="title" className="block w-full p-2 border-2 rounded-md border-black" />
             </fieldset>
-            <fieldset>
-              <label htmlFor="description" className="font-semibold">Description</label>
-              <textarea name="description" value={assignmentData.description} onChange={formData} className="w-full min-h-48 p-2 border-2 rounded-md border-black" required />
+            <fieldset className="flex flex-wrap">
+              <label htmlFor="description" className="flex-1 font-semibold">Description</label> {errors.description && <span className="text-lg text-red-600">{errors.description?.message}</span>}
+              <textarea {...register("description")} id="description" className="block w-full min-h-48 p-2 border-2 rounded-md border-black" />
             </fieldset>
+
             {!assignmentDetails ? (
-              <fieldset>
-                <label htmlFor="employeeAssigned" className="font-semibold">Employee Assigned</label>
-                <select name="employeeAssigned" onChange={formData} className="w-full p-2 border-2 rounded-md border-black" required>
-                  <option value={0} selected>Select an employee</option>
-                  {employeesList ?  employeesList.map((element) => (
+              <fieldset className="flex flex-wrap">
+                <label htmlFor="employeeAssigned" className="flex-1 font-semibold">Employee Assigned</label> {errors.employeeAssigned && <span className="text-lg text-red-600">{errors.employeeAssigned?.message}</span>}
+                <select {...register("employeeAssigned")} id="employeeAssigned" className="block w-full p-2 border-2 rounded-md border-black">
+                  <option value={0} disabled selected>Select an employee</option>
+                  {employeesList ? employeesList.map((element) => (
                     <option value={element.employeeId}>{element.employeeId} - {element.name} {element.lastName}</option>
                   )) : null}
                 </select>
               </fieldset>
             ) : null}
-            <fieldset>
-              <label htmlFor="status" className="font-semibold">Status</label>
-              <div className="flex gap-3">
-                <input name="status" id="incomplete" type="radio" value="planned" checked={assignmentData.status == "planned"} onChange={formData} required />
-                <label htmlFor="incomplete">Planned</label>
-                <input name="status" id="in-progress" type="radio" value="in progress" checked={assignmentData.status == "in progress"} onChange={formData} required />
-                <label htmlFor="in-progress">In progress</label>
-                <input name="status" id="complete" type="radio" value="ongoing" checked={assignmentData.status == "ongoing"} onChange={formData} required />
-                <label htmlFor="complete">Ongoing</label>
+
+            <fieldset className="flex flex-wrap">
+              <label htmlFor="status" className="flex-1 font-semibold">Status</label> {errors.status && <span className="text-lg text-red-600">{errors.status?.message}</span>}
+              <div className="block w-full">
+                <div className="flex gap-3">
+                  <input {...register("status")} type="radio" value="planned" checked={watch("status") == "planned"} />
+                  <label htmlFor="incomplete">Planned</label>
+                  <input {...register("status")} type="radio" value="in progress" checked={watch("status") == "in progress"} />
+                  <label htmlFor="in-progress">In progress</label>
+                  <input {...register("status")} type="radio" value="ongoing" checked={watch("status") == "ongoing"} />
+                  <label htmlFor="complete">Ongoing</label>
+                </div>
               </div>
             </fieldset>
-            <div className="flex flex-col">
-              <label htmlFor="dueAt" className="font-semibold">Expiration Date</label>
-              <input type="date" name="dueAt" value={assignmentData.dueAt} onChange={formData} required className="w-1/2 p-2" />
-            </div>
-            <div className="flex gap-3">
-              <input type="submit" name="Send data" onClick={assignmentDetails == null ? editAssignmentData : newAssignmentData} className="px-6 py-4 text-center rounded-md text-white bg-blue-700 cursor-pointer" />
+            <fieldset className="flex flex-wrap">
+              <label htmlFor="dueAt" className="flex-1 font-semibold">Expiration Date</label> {errors.dueAt && <span className="text-lg text-red-600">{errors.dueAt?.message}</span>}
+              <input type="date" {...register("dueAt")} className="block w-full py-2" />
+            </fieldset>
+            <fieldset className="flex gap-3">
+              <input type="submit" value="Send Data" className="px-6 py-4 text-center rounded-md text-white bg-blue-700 cursor-pointer" />
               <Button buttonName="Close form" buttonColor="red" eventFunction={closeForm} />
-            </div>
+            </fieldset>
           </fieldset>
         </form>
       </article>
