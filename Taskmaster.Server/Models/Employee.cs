9d6ea1f28701e1c9abd7a069d4bf2a20ca293a83
Alan Coiradas <coiradasalan@gmail.com>
2024-07-09 10:42:01 -0300

    Employee.cs: Actualización de modelo

--- Taskmaster.Server/Models/Employee.cs
+++ Taskmaster.Server/Models/Employee.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace Taskmaster.Server.Models;
 
 public partial class Employee
 {
-    [Key]
-    public int EmployeeId { get; set; }
+    public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
+
+    public DateOnly BirthDate { get; set; }
 
     public string Email { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 
-    public string Role { get; set; } = null!;
+    public string Country { get; set; } = null!;
 
-    [JsonIgnore]
-    public virtual ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
+    public string Role { get; set; } = null!;
 }
