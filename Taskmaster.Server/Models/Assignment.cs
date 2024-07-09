76dd960fc6dd12ac53e5a5d9aeea4193e93b4a8c
Alan Coiradas <coiradasalan@gmail.com>
2024-07-09 10:42:16 -0300

    Assignment.cs: Actualización de modelo

--- Taskmaster.Server/Models/Assignment.cs
+++ Taskmaster.Server/Models/Assignment.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace Taskmaster.Server.Models;
 
 public partial class Assignment
 {
-    public Guid AssignmentId { get; set; }
+    public Guid Id { get; set; }
 
-    [ForeignKey("EmployeeId")]
-    public int? EmployeeAssigned { get; set; }
+    public int EmployeeId { get; set; }
 
     public string Title { get; set; } = null!;
 
@@ -18,11 +15,11 @@ public partial class Assignment
 
     public string Status { get; set; } = null!;
 
-    public DateOnly CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
 
-    public DateOnly DueAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 
-    
-    public virtual Employee? EmployeeAssignedNavigation { get; set; }
+    public DateTime DueAt { get; set; }
 
+    public virtual Employee Employee { get; set; } = null!;
 }
