cbfa2ac4a42bef29c6b85cb1f130d178f8fbd727
Alan Coiradas <coiradasalan@gmail.com>
2024-07-12 13:20:01 -0300

    Employee.cs: Añadidos parametros de validación al modelo

--- Taskmaster.Server/Models/Employee.cs
+++ Taskmaster.Server/Models/Employee.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Taskmaster.Server.Models;
 
 public partial class Employee
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
-
+    [Required]
     public string FirstName { get; set; } = null!;
-
+    [Required]
     public string LastName { get; set; } = null!;
-
+    [Required]
     public DateOnly BirthDate { get; set; }
-
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
-
+    [Required]
+    [MinLength(12)]
+    [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{12}$")]
     public string Password { get; set; } = null!;
-
+    [Required]
     public string Country { get; set; } = null!;
-
+    [Required]
+    [AllowedValues("Admin", "Employee")]
     public string Role { get; set; } = null!;
 }
