c9fb3d4d813410f8c9b46315e7534ee6365a20ef
Alan Coiradas <coiradasalan@gmail.com>
2024-07-12 13:20:36 -0300

    Assignment.cs: Añadidos parámetros de validación al modelo

--- Taskmaster.Server/Models/Assignment.cs
+++ Taskmaster.Server/Models/Assignment.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Taskmaster.Server.Models;
 
 public partial class Assignment
 {
+    [Key]
     public Guid Id { get; set; }
-
+    [ForeignKey("Employee")]
     public int EmployeeId { get; set; }
-
+    [Required]
     public string Title { get; set; } = null!;
-
+    [Required]
     public string Description { get; set; } = null!;
-
+    [Required]
+    [AllowedValues("Pending", "In progress", "Completed")]
     public string Status { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
-
+    [Required]
     public DateTime DueAt { get; set; }
 
-    public virtual Employee Employee { get; set; } = null!;
+    public virtual Employee Employee{ get; set; } = null!;
 }
