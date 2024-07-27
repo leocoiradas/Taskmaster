9073379d1fd4bc4dafaef7f460f6b6f8f28257ab
Alan Coiradas <coiradasalan@gmail.com>
2024-07-27 19:13:23 -0300

    Creación de modelo AuthorizeRequest
    Creación de modelo AuthorizeRequest para representar la petición de inicio de sesión, contiene las propiedades Email y Password que se verificarán con los métodos correspondientes de autenticación.

---
+++ Taskmaster.Server/Models/JWTAdminClasses/AuthorizeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Taskmaster.Server.Models.JWTAdminClasses
+{
+    public class AuthorizeRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
