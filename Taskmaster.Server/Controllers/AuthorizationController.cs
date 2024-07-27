837335a2f9fa5526e71ef0b730ba0634f66125b1
Alan Coiradas <coiradasalan@gmail.com>
2024-07-27 20:17:02 -0300

    Creación del controlador de autorización
    Se creó el controlador dedicado a la autorización del usuario con un método correspondiente a validar las credenciales del mismo y generar un token para que navegue por la aplicación.

---
+++ Taskmaster.Server/Controllers/AuthorizationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taskmaster.Server.Models.JWTAdminClasses;
+using Taskmaster.Server.Services;
+
+namespace Taskmaster.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController(IAuthorizeService authorizeService) : ControllerBase
+    {
+        private readonly IAuthorizeService _authorizeService = authorizeService;
+
+        [HttpPost]
+        [Route("Authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] AuthorizeRequest request)
+        {
+            var authorizeResult = await _authorizeService.GetToken(request);
+            if (authorizeResult == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(authorizeResult);
+        }
+    }
+
+}
