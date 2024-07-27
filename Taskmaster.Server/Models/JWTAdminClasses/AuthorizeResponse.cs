db368fdddb8527c977591ae3c4bbd414ffa7859d
Alan Coiradas <coiradasalan@gmail.com>
2024-07-27 19:15:33 -0300

    Creación de modelo AuthorizeResponse
    Este modelo representa la respuesta que se dará al momento de generar una petición de autenticación en la aplicación. Contiene propiedades que representan al Token que se usará para verificar si el usuario está autenticado, la propiedad Success que representa el éxito de la autenticación y una propiedad Message para describir el estado de la petición.

---
+++ Taskmaster.Server/Models/JWTAdminClasses/AuthorizeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskmaster.Server.Models.JWTAdminClasses
+{
+    public class AuthorizeResponse
+    {
+        public string Token { get; set; }
+        public bool  Success { get; set; }
+        public string Message { get; set; }
+    }
+}
