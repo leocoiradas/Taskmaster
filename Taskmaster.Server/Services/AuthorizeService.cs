0977c3562924dd0c077753fb9ce59f8a00704e6b
Alan Coiradas <coiradasalan@gmail.com>
2024-07-27 20:13:38 -0300

    Creación de AuthorizeService
    En esta clase se declaran dos métodos:
    
    - GenerateToken: Este método se encarga de escribir un token a partir de una clave secreta, codificar la llave y asociarla al email del usuario. Dicho token tiene validez por un día.
    
    - GetToken: Este método busca un usuario acorde al email ingresado en el objeto de clase AuthorizeRequest, acto seguido, si lo encuentra, verifica que la contraseña ingresada coincida con la contraseña presente en la base de datos (ambas encriptadas y se usa Bcrypt para decodificar las contraseñas y compararlas). Si tanto el correo y la contraseña son válidos, se crea un token de uso para que el usuario tenga acceso a la aplicación, caso contrario o que no se encuentre un usuario por su email, se devuelve una respuesta del tipo Unauthorized.

---
+++ Taskmaster.Server/Services/AuthorizeService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Taskmaster.Server.Models;
+using Taskmaster.Server.Models.JWTAdminClasses;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taskmaster.Server.Services
+{
+    public class AuthorizeService(TaskmasterContext dbcontext, IConfiguration configuration) : IAuthorizeService
+    {
+        private readonly TaskmasterContext _dbcontext = dbcontext;
+        private readonly IConfiguration _configuration = configuration;
+
+        private string GenerateToken(string userEmail)
+        {
+            var tokenKey = _configuration.GetSection("JwtSettings").GetSection("key").ToString();
+            var keyBytes = Encoding.ASCII.GetBytes(tokenKey);
+            var claims = new ClaimsIdentity();
+            claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, userEmail));
+
+            var TokenCredential = new SigningCredentials(
+                new SymmetricSecurityKey(keyBytes),
+                SecurityAlgorithms.HmacSha256Signature);
+            var TokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddDays(1),
+            };
+
+            var TokenHandler = new JwtSecurityTokenHandler();
+            var tokenConfig = TokenHandler.CreateToken(TokenDescriptor);
+            string createdToken = TokenHandler.WriteToken(tokenConfig);
+
+            return createdToken;
+        }
+
+        public async Task<AuthorizeResponse> GetToken(AuthorizeRequest request)
+        {
+
+            try
+            {
+                var userFound = _dbcontext.Employees.FirstOrDefault(x => x.Email == request.Email);
+                if (userFound == null || !BCrypt.Net.BCrypt.Verify(request.Password, userFound.Password))
+                {
+                    return await Task.FromResult<AuthorizeResponse>(null);
+                }
+                string createdToken = GenerateToken(userFound.Email.ToString());
+                return new AuthorizeResponse() { Token = createdToken, Success = true, Message = "Token received successfully" };
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error);
+
+                return new AuthorizeResponse { Success = false, Message = "Error while proccessing the request" };
+            }
+        }
+    }
+}
