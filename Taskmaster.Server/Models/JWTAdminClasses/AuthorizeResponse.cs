f1ef8f5ba6341cc515bd10de6706b68d69e5912f
Alan Coiradas <coiradasalan@gmail.com>
2024-07-29 18:31:20 -0300

    AuthorizeResponse: Agregada propiedad User
    Se agregó una propiedad User de la clase EmployeeDTO. Esta propiedad contiene algunos detalles no sensibles del usuario como su nombre, correo y rol.

--- Taskmaster.Server/Models/JWTAdminClasses/AuthorizeResponse.cs
+++ Taskmaster.Server/Models/JWTAdminClasses/AuthorizeResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace Taskmaster.Server.Models.JWTAdminClasses
+﻿using Taskmaster.Server.DTO;
+
+namespace Taskmaster.Server.Models.JWTAdminClasses
 {
     public class AuthorizeResponse
     {
         public string Token { get; set; }
+        public EmployeeDTO User {  get; set; }
         public bool  Success { get; set; }
         public string Message { get; set; }
     }
