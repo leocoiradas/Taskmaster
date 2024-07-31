65248a264303773b65ba6fafd3899077a433b4c2
Alan Coiradas <coiradasalan@gmail.com>
2024-07-31 19:50:14 -0300

    router: implementación de ApplicationView, ProtectedRoute y RedirectEmployee

--- taskmaster.client/src/router/router.jsx
+++ taskmaster.client/src/router/router.jsx
@@ -6,37 +6,53 @@ import AssignmentsByEmployee from "../Layouts/AssignmentsByEmployee";
 import EmployeesCollection from "../Layouts/EmployeesCollection";
 import LoginForm from "../Components/UserSessionsComponents/LoginForm";
 import RegistrationForm from "../Components/UserSessionsComponents/RegistrationForm";
+import ProtectedRoute from "./ProtectedRoute";
+import RedirectEmployee from "./RedirectEmployee";
+import ApplicationView from "../Layouts/ApplicationView";
 
 export const router = createBrowserRouter([
     {
-        path: "",
-        element: <WelcomeHomepage />,
-    },
-    {
-        path: "/login",
-        element: <LoginForm />
-    },
-    {
-        path: "/register",
-        element: <RegistrationForm />
-    },
-    {
-        path: "/dashboard",
-        element: <Layout />,
+        path: "/",
+        element: <ApplicationView />,
         children: [
             {
-                path: "",
-                element: <AssignmentCollection />
+                path: "/",
+                element: <WelcomeHomepage />,
+            },
+            {
+                path: "/login",
+                element: <RedirectEmployee path="/dashboard">
+                    <LoginForm />
+                </RedirectEmployee>
+            },
+            {
+                path: "/register",
+                element: <RedirectEmployee path="/dashboard">
+                    <RegistrationForm />
+                </RedirectEmployee>
+            },
+            {
+                path: "/dashboard",
+                element: <ProtectedRoute path="/login"><Layout /></ProtectedRoute>,
+                children: [
+                    {
+                        path: "",
+                        element: <AssignmentCollection />
 
-            },
-            {
-                path: "myAssignments",
-                element: <AssignmentsByEmployee />
-            },
-            {
-                path: "/dashboard/employees",
-                element: <EmployeesCollection />
+                    },
+                    {
+                        path: "myAssignments",
+                        element: <AssignmentsByEmployee />
+                    },
+                    {
+                        path: "/dashboard/employees",
+                        element: <EmployeesCollection />
+
+                    }
+                ]
             }
         ]
-    }])
+    }
+
+])
 
