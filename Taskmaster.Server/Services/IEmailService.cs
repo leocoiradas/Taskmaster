99f91f162ce33a933b656964fc36e74b15e2cd02
Alan Coiradas <coiradasalan@gmail.com>
2024-08-07 22:31:21 -0300

    Creación de interface con declaración de método de enviar correo

---
+++ Taskmaster.Server/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taskmaster.Server.Services
+{
+    public interface IEmailService
+    {
+        void SendEmailAsync(string email, string password);
+    }
+}
